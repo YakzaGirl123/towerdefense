e0dd644fc374791fe71008fe36eb6c8d1b76ef28
YakzaGirl123 <116183846+YakzaGirl123@users.noreply.github.com>
2022-12-25 15:43:25 +0700

    Add files via upload

---
+++ Tower.lua
@@ -0,0 +1,199 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/banbuskox/dfhtyxvzexrxgfdzgzfdvfdz/main/ckmhjvskfkmsStratFun2", true))() 
+
+TDS:Loadout("Scout", "Sniper", "nil", "nil", "nil") 
+
+TDS:Map("Portland", true, "Survival")
+
+TDS:Mode('Normal')--Starter Grind by #Nguyen Hua Minh Quân
+
+TDS:Place('Scout', -21.07355499267578, 0.2301158905029297, 18.69173812866211, 0, 0, 24)
+
+TDS:Upgrade(1, 0, 0, 23)
+
+TDS:Place('Scout', -17.022647857666016, 0.22975444793701172, 18.743301391601562, 0, 0, 11)
+
+TDS:Upgrade(2, 0, 0, 10)
+
+TDS:Place('Scout', -13.26114273071289, 0.22942161560058594, 19.22350311279297, 2, 0, 58)
+
+TDS:Upgrade(3, 2, 0, 57)
+
+TDS:Place('Sniper', -9.475704193115234, 7.751002311706543, 15.617055892944336, 3, 0, 57)
+
+TDS:Upgrade(4, 3, 0, 5)
+
+TDS:Upgrade(3, 4, 0, 59)
+
+TDS:Upgrade(2, 4, 0, 1)
+
+TDS:Upgrade(1, 5, 0, 4)
+
+TDS:Upgrade(4, 6, 0, 59)
+
+TDS:Place('Sniper', -13.272991180419922, 7.751002311706543, 14.758727073669434, 7, 0, 58)
+
+TDS:Upgrade(5, 7, 0, 57)
+
+TDS:Upgrade(5, 7, 0, 3)
+
+TDS:Place('Scout', -24.14844512939453, 0.23038768768310547, 19.097640991210938, 8, 0, 4)
+
+TDS:Upgrade(6, 8, 0, 3)
+
+TDS:Upgrade(6, 8, 0, 2)
+
+TDS:Place('Sniper', -16.596965789794922, 7.751002311706543, 14.68368148803711, 9, 0, 58)
+
+TDS:Upgrade(7, 9, 0, 57)
+
+TDS:Place('Sniper', -5.745538711547852, 7.751002311706543, 15.332261085510254, 9, 0, 52)
+
+TDS:Upgrade(8, 9, 0, 47)
+
+TDS:Upgrade(7, 10, 0, 59)
+
+TDS:Upgrade(8, 10, 0, 48)
+
+TDS:Upgrade(8, 11, 0, 4)
+
+TDS:Upgrade(4, 12, 0, 4)
+
+TDS:Upgrade(5, 14, 0, 55)--Starter Grind by #LackOfWifi#1723
+
+TDS:Upgrade(7, 14, 0, 2)
+
+TDS:Upgrade(3, 15, 0, 1)
+
+TDS:Upgrade(2, 16, 0, 58)
+
+TDS:Upgrade(1, 16, 0, 4)
+
+TDS:Upgrade(6, 16, 0, 3)
+
+TDS:Upgrade(1, 18, 0, 3)
+
+TDS:Place('Sniper', -20.1894588470459, 7.751002311706543, 15.518404006958008, 19, 0, 28)
+
+TDS:Upgrade(9, 19, 0, 27)
+
+TDS:Upgrade(9, 19, 0, 5)
+
+TDS:Upgrade(9, 19, 0, 4)
+
+TDS:Place('Sniper', -9.377171516418457, 7.751002311706543, 12.409147262573242, 20, 0, 57)
+
+TDS:Upgrade(10, 20, 0, 55)
+
+TDS:Upgrade(10, 20, 0, 55)
+
+TDS:Upgrade(10, 20, 0, 4)
+
+TDS:Place('Sniper', -33.24842834472656, 7.751002311706543, 14.234642028808594, 21, 0, 37)
+
+TDS:Upgrade(11, 21, 0, 36)
+
+TDS:Upgrade(11, 21, 0, 36)
+
+TDS:Upgrade(11, 21, 0, 4)
+
+TDS:Place('Sniper', -1.9295330047607422, 7.751002311706543, 15.389253616333008, 22, 0, 1)
+
+TDS:Upgrade(12, 22, 0, 58)
+
+TDS:Upgrade(12, 22, 0, 58)
+
+TDS:Upgrade(12, 22, 0, 27)
+
+TDS:Place('Sniper', 1.612715721130371, 7.751002311706543, 15.5001859664917, 22, 0, 10)
+
+TDS:Upgrade(13, 22, 0, 8)
+
+TDS:Upgrade(13, 22, 0, 8)
+
+TDS:Upgrade(13, 22, 0, 5)
+
+TDS:Place('Sniper', 1.8282337188720703, 7.751002311706543, 12.432242393493652, 23, 0, 46)
+
+TDS:Upgrade(14, 23, 0, 44)
+
+TDS:Upgrade(14, 23, 0, 44)--Starter Grind by #LackOfWifi#1723
+
+TDS:Upgrade(14, 23, 0, 28)
+
+TDS:Place('Sniper', -37.13174057006836, 7.751002311706543, 12.133678436279297, 23, 0, 4)
+
+TDS:Upgrade(15, 23, 0, 2)
+
+TDS:Upgrade(15, 23, 0, 2)
+
+TDS:Upgrade(15, 23, 0, 1)
+
+TDS:Place('Sniper', -36.703330993652344, 7.751002311706543, 15.853965759277344, 24, 0, 54)
+
+TDS:Upgrade(16, 24, 0, 53)
+
+TDS:Upgrade(16, 24, 0, 48)
+
+TDS:Upgrade(16, 24, 0, 5)
+
+TDS:Place('Sniper', -32.83076858520508, 7.159343242645264, -4.1754150390625, 25, 0, 47)
+
+TDS:Upgrade(17, 25, 0, 46)
+
+TDS:Upgrade(17, 25, 0, 45)
+
+TDS:Upgrade(17, 25, 0, 5)
+
+TDS:Place('Sniper', -20.11481285095215, 7.751002311706543, 12.399961471557617, 26, 0, 55)
+
+TDS:Upgrade(18, 26, 0, 54)
+
+TDS:Upgrade(18, 26, 0, 53)
+
+TDS:Upgrade(18, 26, 0, 53)
+
+TDS:Place('Sniper', -40.72605514526367, 7.751002311706543, 12.25623607635498, 27, 0, 56)
+
+TDS:Upgrade(19, 27, 0, 55)
+
+TDS:Upgrade(19, 27, 0, 54)
+
+TDS:Upgrade(19, 27, 0, 54)
+
+TDS:Place('Sniper', -40.554710388183594, 7.751002311706543, 15.897595405578613, 27, 0, 49)
+
+TDS:Upgrade(20, 27, 0, 47)
+
+TDS:Upgrade(20, 27, 0, 47)
+
+TDS:Upgrade(20, 27, 0, 38)
+
+TDS:Place('Sniper', 5.973940372467041, 7.751002311706543, 15.4154691696167, 28, 0, 52)
+
+TDS:Upgrade(21, 28, 0, 51)
+
+TDS:Upgrade(21, 28, 0, 51)
+
+TDS:Upgrade(21, 28, 0, 50)
+
+TDS:Place('Sniper', -29.16613006591797, 7.159438133239746, -7.509609222412109, 28, 0, 36)
+
+TDS:Sell(22, 28, 0, 35)
+
+TDS:Place('Sniper', -29.516433715820312, 7.159419059753418, -7.196532726287842, 28, 0, 31)
+
+TDS:Upgrade(23, 28, 0, 30)
+
+TDS:Upgrade(23, 28, 0, 30)
+
+TDS:Upgrade(23, 28, 0, 29)--Starter Grind by #LackOfWifi#1723
+
+TDS:Place('Sniper', 9.378538131713867, 7.751002311706543, 12.852798461914062, 28, 0, 15)
+
+TDS:Upgrade(24, 28, 0, 14)
+
+TDS:Upgrade(24, 28, 0, 14)
+
+TDS:Upgrade(24, 28, 0, 14)
+
+TDS:Place('Sniper', -36.41805648803711, 7.159343242645264, -5.21421480178833, 29, 0, 54)
